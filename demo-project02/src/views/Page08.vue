4b299377c4fdad6207ead89b45020431a7ed1842
QJ殷鹏飞 <1207828715@qq.com>
2020-10-21 10:44:07 +0800

    blob

--- demo-project02/src/views/Page08.vue
+++ demo-project02/src/views/Page08.vue
@@ -1,18 +1,21 @@
 <!--
  * @Date: 2020-10-12 13:46:32
- * @information: 
+ * @information: Blob
 -->
 <template>
   <div id="page08">
+
+
 
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-
+      path: `https://finance-images-test.oss-us-west-1.aliyuncs.com/h516031808960008776`,
     }
   },
   methods: {
@@ -20,7 +23,24 @@ export default {
 
 
   },
-  created() {
+  async created() {
+
+
+  let blob = new Blob(['Hello world'], {
+    type: 'text/html,charset=UTF-8'   
+  })
+  console.log(blob)
+  let objectURL = URL.createObjectURL(blob);
+  console.log(objectURL)
+
+  const link = document.createElement('a')
+  link.style.display = 'none'
+  link.href = objectURL
+  link.setAttribute('download', 'aa.txt')
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+
 
   },
 }
