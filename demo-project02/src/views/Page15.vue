9a7598ae741cdcb6b8a4dffed47c1a3380b7e4ac
yinpengfei <1207828715@qq.com>
2021-06-23 16:02:32 +0800

    page15 moment方法使用

--- demo-project02/src/views/Page15.vue
+++ demo-project02/src/views/Page15.vue
@@ -1,21 +1,41 @@
 <!--
  * @Date: 2021-05-25 18:15:58
- * @information: 使用scss封装阴影渐变
+ * @information:  使用scss封装阴影渐变
+                  moment方法使用 - http://momentjs.cn/docs/#/manipulating
 -->
 <template>
   <div id="container">
     <div class="box"></div>
 
     <div class="dynamic-shadow"></div>
-  
-  
-  
+
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+// 引入moment中文包
+import 'moment/locale/zh-cn'
+
 export default {
-  
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    moment,
+    useMomentMethod () {
+      console.log('moment上的方法 ->', moment.prototype)
+      let a = moment('2021-06-23 10:05:46').fromNow()
+      let b = moment().startOf('day')
+      let c = moment().endOf('month')
+      console.log(a, b, c)
+    },
+  },
+  mounted () {
+    this.useMomentMethod()
+  },
 }
 </script>
 
@@ -34,7 +54,6 @@ export default {
 
   box-shadow: 3px 3px 10px #ff6700;
   background-image: linear-gradient(to right, #ff0000, #8a08f5);
-
 }
 
 .dynamic-shadow {
@@ -42,8 +61,5 @@ export default {
   height: 10rem;
   margin: 50px auto 0;
   @include linear-shadow(to bottom, #ff6700, #003cff);
-
-
 }
-
 </style>
